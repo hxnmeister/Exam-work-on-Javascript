ec79a263ca35a58ca7c79ed0a5e68bea8741e664
Valerii <valeriy.stronskiy@gmail.com>
2023-07-24 16:16:24 +0300

    Exam is done!
    The task implemented a connection to the "rickandmortyapi" API and getting information about the characters and the episodes in which they appeared.
    
    When you click on the "Search" button, a check will be made for emptiness and the correctness of the text input, if emptiness was entered, not a number, an incorrect number, or a character with the same id has already been added, the user will receive a corresponding error. After a successful check, the page will display the characters whose id the user has entered.
    
    When you click on the "Delete" button, the selected character is removed from the page. When clicking on a picture with a character, the user is taken to a page with detailed information about the character and the episodes in which he appeared.
    
    The user has the option to return to the home page by clicking on the "Home" button. After returning to the main page, all the characters that were previously uploaded by the user are displayed.

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-let characters = [];
-
-const form = document.forms.search;
-const searchText = form.searchtext;
-const charactersList = document.getElementById("characterslist");
 const charactersApiUrl = "https://rickandmortyapi.com/api/character";
 
+const form = document.forms.searchform;
+const searchText = form.searchtext;
+
+const searchBar = document.getElementById("searchbar");
+const charactersList = document.getElementById("characterslist");
+const charactersContainer = document.getElementById("characters");
+
+// localStorage.clear();
+
+const addCharcterOnPage = (character) =>
+{
+    charactersList.innerHTML += 
+    `
+        <div class="frame">
+            <img src="${character.image}" alt="characterpicture">
+            <p>${character.name}</p>
+            <button id="${character.id}">Delete</button>
+        </div>
+    `
+}
 const checkCharactersInList = (character) =>
 {
-    if(!characters.find(item => item.id == character.id))
+    if(!localStorage.getItem(character.id))
     {
-        characters.push(character);
-        charactersList.innerHTML += 
-        `
-            <div class="frame">
-                <img src="${character.image}" alt="characterpicture">
-                <p>${character.name}</p>
-                <button id="${character.id}">Delete</button>
-            </div>
-        `
+        localStorage.setItem(character.id, JSON.stringify(character));
+        addCharcterOnPage(character);
     }
     else
     {
@@ -31,35 +39,27 @@ form.addEventListener("submit", async (event) =>
 
     try
     {
-        const requestResult = await fetch(`${charactersApiUrl}/${searchText.value}`).then(async result => 
+        if(searchText.value !== "")
+        {
+            const requestResult = await fetch(`${charactersApiUrl}/${searchText.value}`).then(async result => 
             {
                 return result.ok ? result.json() : result.json().then(errorMessge => { throw new Error(errorMessge.error) });
             });
-        
-            if(searchText.value !== "")
+
+            if(requestResult.length == undefined)
             {
-                if(requestResult.length == undefined)
-                {
-                    checkCharactersInList(requestResult);
-                    // const currentCharacters = charactersList.querySelectorAll("button");
-                    // currentCharacters.forEach(button => 
-                    // {
-                    //     if(button.id === String(requestResult.id))
-                    //     {
-                    //         button.addEventListener("click", (event) => alert(event.currentTarget.id));
-                    //     }
-                    // });
-                }
-                else
-                {
-                    requestResult.map(item => checkCharactersInList(item));
-                }
+                checkCharactersInList(requestResult);
             }
             else
             {
-                throw new Error("Search field was empty!");
+                requestResult.map(item => checkCharactersInList(item));
             }
         }
+        else
+        {
+            throw new Error("Search field was empty!");
+        }
+    }
     catch(e)
     {
         alert(e.message);
@@ -71,21 +71,18 @@ charactersList.addEventListener("click", event =>
 
     if(deleteButton !== null)
     {
-        const position = characters.findIndex(item => String(item.id) === deleteButton.id);
-    
-        if(confirm(`Confirm character ${characters[position].name} deletion?`))
+        if(confirm(`Confirm character ${JSON.parse(localStorage.getItem(deleteButton.id)).name} deletion?`))
         {
-            characters.splice(position, 1);
-    
+            localStorage.removeItem(deleteButton.id);
+
             charactersList.innerHTML = "";
-            characters.map(character => charactersList.innerHTML += 
-            `
-                <div class="frame">
-                    <img src="${character.image}" alt="characterpicture">
-                    <p>${character.name}</p>
-                    <button id="${character.id}">Delete</button>
-                </div>
-            `);
+            for(let i = 0; i < localStorage.length; i++)
+            {
+                const key = localStorage.key(i);
+                const character = JSON.parse(localStorage.getItem(key));
+
+                addCharcterOnPage(character);
+            }
         }
     }
 });
@@ -96,29 +93,21 @@ charactersList.addEventListener("click", async event =>
     if(choosedCharacter !== null)
     {
         const choosedCharacterId = choosedCharacter.parentNode.getElementsByTagName("button")[0].id;
-        const position = characters.findIndex(item => String(item.id) === choosedCharacterId);
-        let characterInfo = 
-        `
-            <div id="characterinfocontainer">
-                <img src="${characters[position].image}" alt="portrait">
-                <p id="chrname">Name: <b>${characters[position].name}</b></p>
-                <p><u>Episodes: </u></p>
-        `;
+        localStorage.setItem("choosedCharacterId", choosedCharacterId);
 
-        for (const episodeUrl of characters[position].episode) 
-        {
-            await fetch(episodeUrl).then(response => response.json()).then(result => characterInfo += 
-                `
-                    <div class="episode"> 
-                    <p><b>Name: </b>${result.name}</p>
-                    <p><b>Episode: </b>${result.episode}</p>
-                    <p><b>Air date: </b>${result.air_date}</p>
-                    </div>
-                `);
-        }
-
-        characterInfo += "</div>";
-
-        charactersList.innerHTML = characterInfo;
+        window.location.replace("/index1.html");
     }
 })
+window.addEventListener("load", () => 
+{
+    if(charactersList.innerHTML === "" && localStorage.length > 0)
+        {
+            for(let i = 0; i < localStorage.length; i++)
+            {
+                const key = localStorage.key(i);
+                const character = JSON.parse(localStorage.getItem(key));
+
+                if(key !== "choosedCharacterId") addCharcterOnPage(character);
+            }
+        }
+});
---
+++ script1.js
@@ -0,0 +1,53 @@
+const characterFullInfo = document.getElementById("characterfullinfo");
+
+const getCharacterFullInfo = async (character) =>
+{
+    characterFullInfo.innerHTML =
+    `
+        <div id="controllelement">
+            <button id="homebutton">Home</button>
+        </div>
+        <div id="biography">
+            <img src="${character.image}" alt="portrait">
+            <p><b>Name: </b>${character.name}</p>
+            <p><b>Origin: </b>${character.origin.name}</p>
+            <p><b>Gender: </b>${character.gender}</p>
+            <p><b>Species: </b>${character.species}</p>
+            <p><b>Current status: </b>${character.status}</p>
+        </div>
+    `
+    characterFullInfo.innerHTML +=
+    `
+        <div id="episodes">
+            <p><u>Episodes:</u></p>
+            <br>
+        </div>
+    `
+
+    for(const episodeUrl of character.episode)
+    {
+        await fetch(episodeUrl).then(response => response.json()).then(episodeInfo => 
+        {
+            characterFullInfo.querySelector("#episodes").innerHTML +=
+            `
+                <div class="episode">
+                    <p><b>Title: </b>${episodeInfo.name}</p>
+                    <p><b>Episode: </b>${episodeInfo.episode}</p>
+                    <p><b>Air date: </b>${episodeInfo.air_date}</p>
+                </div>
+            `
+        });
+    }
+}
+
+window.addEventListener("load", () => getCharacterFullInfo(JSON.parse(localStorage.getItem(localStorage.getItem("choosedCharacterId")))));
+characterFullInfo.addEventListener("click", (event) => 
+{
+    const button = event.target.closest("button");
+
+    if(button !== null)
+    {
+        localStorage.removeItem("choosedCharacterId");
+        window.location.replace("/index.html");
+    }
+});
